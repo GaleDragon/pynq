45911c2213aebb1b0ab1245e45c82728e4c27cd1
Jeremy Morgan <galedragon1223@gmail.com>
2014-04-30 11:28:23 -0400

    added chaining ability

--- pynq/__init__.py
+++ pynq/__init__.py
@@ -1,20 +1,31 @@
 import itertools
 
 class Pynq (object):
+    def _convert(self, x):
+        if isinstance(x, list):
+            return List(x)
+        elif isinstance(x, dict):
+            return Group(x)
+        elif isinstance(x, str):
+            return String(x)
+        else:
+            return x
+    
     def select(self, fn):
-        return [fn(x) for x in self]
+        l = List([fn( self._convert(x) ) for x in self])
+        return l
         
     def group(self, fn):
         # [k for k, g in groupby('AAAABBBCCDAABBB')] --> A B C D A B
         # [list(g) for k, g in groupby('AAAABBBCCD')] --> AAAA BBB CC D
         # { g1 : (), g2 : () }
-        ret = {}
+        ret = Group()
         for x in self:
-            r = fn(x)
+            r = fn( self._convert(x) )
             if r not in ret.keys():
-                ret[r] = [x]
+                ret[r] = List([ self._convert(x) ])
             else:
-                ret[r].append(x)
+                ret[r].append( self._convert(x) )
         return ret
     
     def aggregate(self, fn):
@@ -27,10 +38,39 @@ class Pynq (object):
         return s
     
     def average(self, fn):
-        pass
+        s = sum( self._convert([ fn(x) for x in self ]) )
+        return s / len(self)
+        
+    def __getattr__(self, name):
+        self = self._convert(self)
+        super.__getattr__(name)
         
 class List (list, Pynq):
     def __init__(self, *args, **kwargs):
-        if isinstance(args[0], list):
+        if len(args) == 0:
+            pass
+        elif isinstance(args[0], list):
             for a in args[0]:
                 self.append(a)
+                
+class Group (dict, Pynq):
+    def __init__(self, *args, **kwargs):
+        if len(args) == 0:
+            pass
+        elif isinstance(args[0], dict):
+            for a in args[0]:
+                self[args[0]] = args[0][a]
+                
+class String (str, Pynq):
+    def __init__(self, *args, **kwargs):
+        if len(args) == 0:
+            pass
+        elif isinstance(args[0], str):
+            for a in args[0]:
+                self += a
+                
+                
+                
+                
+                
+                
--- select.py
+++ select.py
@@ -2,10 +2,8 @@ from pynq import List
 import unittest
 
 class SelectTest (unittest.TestCase):
-    
     def setUp(self):
         self.l = List( [1, 2, 3, 4, 5 ] )
-        
     def test_select(self):
         a = self.l.select(lambda x: x-1)
         self.assertEquals(a, [0,1,2,3,4])
@@ -13,11 +11,22 @@ class SelectTest (unittest.TestCase):
 class GroupCase (unittest.TestCase):
     def setUp(self):
         self.l = List(["aardvark", "apple", "bear", "awesome", "bone"])
-    
     def test_group(self):
         r = self.l.group(lambda x: x[0])
-        print r
         self.assertEquals( r.keys(), ["a", "b"] )
+        
+class ChainCase (unittest.TestCase):
+    def setUp(self):
+        self.l = List([0,2,3,6,7,4,9,8,10])
+        self.r = List(["aardvark", "apple", "bear", "awesome", "bone"])
+    def test_chaining(self):
+        r = self.l.select(lambda x: x-2).group(lambda y: y%2)
+        self.assertEquals(r, {0: [-2, 0, 4, 2, 6, 8], 1:[1, 5, 7]})
+        
+        s = self.r.group(lambda x: x[0]).select(lambda y: s[y].average( lambda x: len(x) ) )
+        self.assertFalse(False)
+        
+        
         
 if __name__=="__main__":
     unittest.main()
